4dbac12756c3b96f3ed20e06faa41d415c6f5d30
Walmir Marques Filho <walmir-mf@hotmail.com>
2021-04-04 20:44:58 -0300

    Primeiro commit! Projeto de um churrascometro.

---
+++ README.md
@@ -0,0 +1,7 @@
+Projeto de um planejador de churrasco, usando de javascript para calcular o consumo de carne, cerveja e refrigerante/agua. 
+
+// Carne - 400 gr por pessoa + de 6 horas - 650 gr
+// Cerveja - 1200 ml por Pessoa + 6 horas - 2000ml
+// Refrigerante/agua - 1000ml por pessoa + 6horas 1500 ml
+
+// crianças valem 0,5
---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+// Carne - 400 gr por pessoa + de 6 horas - 650 gr
+// Cerveja - 1200 ml por Pessoa + 6 horas - 2000ml
+// Refrigerante/agua - 1000ml por pessoa + 6horas 1500 ml
+
+// crianças valem 0,5
+
+let numAdultos = document.getElementById("adultos").value;
+let numCriancas = document.getElementById("criancas").value;
+let numDuracao = document.getElementById("duracao").value;
+
+
+function calculaChurrascometro(){
+    let numAdultos = document.getElementById("adultos").value;
+    let numCriancas = parseInt(document.getElementById("criancas").value / 2);
+    let numDuracao = document.getElementById("duracao").value; 
+
+    let carne;
+    let cerveja;
+    let refrigerante;
+
+    if (numDuracao >= 0 && numDuracao <= 5) {
+        carne = (400 * numAdultos) * numCriancas;
+        cerveja = (1200 * numAdultos);
+        refrigerante = (1000 * numAdultos) * numCriancas;
+    } else {
+        carne = (650 * numAdultos) * numCriancas;
+        cerveja = (2000 * numAdultos);
+        refrigerante = (1500 * numAdultos) * numCriancas;
+    }
+
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<p> ${carne} grms de carne para o seu churrasco!!! </p>`
+    resultado.innerHTML += `<p> ${cerveja} mls de cerveja para o seu churrasco!!! </p>`
+    resultado.innerHTML += `<p> ${refrigerante} mls de Refrigerante/Água para o seu churrasco!!! </p>`
+}
